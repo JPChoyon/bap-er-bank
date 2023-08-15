9afb18875cf6c51d2c1142d61b701af8722f8a29
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-08-16 01:34:52 +0600

    done

--- js/dipositAndWithdraw.js
+++ js/dipositAndWithdraw.js
@@ -1,30 +1,41 @@
 // diposit function 
 
-document.getElementById('diposit-btn').addEventListener('click', function(){
+document.getElementById('diposit-btn').addEventListener('click', function () {
     const dipositField = document.getElementById('diposit-input');
     const dipositValue = parseFloat(dipositField.value);
-   
+
     const dipositElement = document.getElementById('diposit-current');
     const currentDiposit = parseFloat(dipositElement.innerText);
 
     const totallDiposit = dipositValue + currentDiposit;
     dipositElement.innerText = totallDiposit;
-    
+
+    const balanceField = document.getElementById('balance');
+    const balanceValue = parseFloat(balanceField.innerText);
+    const newBalance = balanceValue + dipositValue;
+    balanceField.innerText =newBalance;
+
 
     dipositField.value = '';
 })
 
 // Withdraw function 
 
-document.getElementById('withdraw-btn').addEventListener('click', function(){
+document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-input');
-    const withdrawValue =parseFloat(withdrawField.value) ;
-    
+    const withdrawValue = parseFloat(withdrawField.value);
+
     const withdrawElement = document.getElementById('withdraw-current');
     const currentWithdraw = parseFloat(withdrawElement.innerText);
 
     const totallWithdraw = withdrawValue + currentWithdraw;
     withdrawElement.innerText = totallWithdraw;
 
-    withdrawField.value ='';
+    const balanceField = document.getElementById('balance');
+    const balanceValue = parseFloat(balanceField.innerText);
+    const newBalance = balanceValue - withdrawValue;
+    balanceField.innerText =newBalance;
+
+
+    withdrawField.value = '';
 })
